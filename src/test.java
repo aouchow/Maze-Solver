875fad61c5640ea9b60319bbc78ced6a2c4ddd1c
Audrey <aouchow@gmail.com>
2020-02-02 14:52:30 -0500

    test

---
+++ src/test.java
@@ -0,0 +1,15 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		PathNode[][] testMap = StaticGenAndSearch.generateMap(4, 0.5);
+		StaticGenAndSearch.printMap(testMap);
+		System.out.println();
+		PathNode goal = StaticGenAndSearch.DepthFirstSearch(testMap);
+		while (goal!= null) {
+			System.out.println("Node: row - " + goal.row + " col - " + goal.col); 
+			goal = goal.prev;
+		}
+	}
+
+}
