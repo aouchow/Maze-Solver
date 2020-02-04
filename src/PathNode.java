f64e519b3cbea60a4274d9b8eee409108fe1c576
wamitai <34920740+wamitai@users.noreply.github.com>
2020-02-04 11:11:34 -0500

    added distance parameter for A*

--- src/PathNode.java
+++ src/PathNode.java
@@ -2,12 +2,14 @@
 public class PathNode {
 	int row;
 	int col;
+	int distance;
 	boolean isEmpty; //true if Node is safe to move to
 	PathNode prev;
 	
 	public PathNode(int row, int col, boolean isEmpty){
 		this.row = row;
 		this.col = col;
+		this.distance = 0;
 		this.isEmpty = isEmpty;
 		this.prev = null;
 	}
